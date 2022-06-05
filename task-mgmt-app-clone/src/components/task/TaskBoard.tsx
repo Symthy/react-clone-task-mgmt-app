00f9bf157df27e1d135822e04025725171ad5607
Symthy <sympathia3104@gmail.com>
2022-06-05 18:04:44 +0900

    add: can drag task card

--- task-mgmt-app-clone/src/components/task/TaskBoard.tsx
+++ task-mgmt-app-clone/src/components/task/TaskBoard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 import { AddTaskCardBtn } from './button/AddTaskCardBtn';
 import { TaskCard } from './TaskCard';
 import styles from './TaskBoard.module.scss';
@@ -22,12 +23,33 @@ export const TaskBoard = () => {
     setTaskCards(newTaskCards);
   };
 
+  const reorderTaskCards = (startIndex: number, endIndex: number) => {
+    const remove = taskCards.splice(startIndex, 1);
+    taskCards.splice(endIndex, 0, remove[0]);
+    setTaskCards(taskCards);
+  };
+
+  const onDragEndSortTasks = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+    reorderTaskCards(result.source.index, result.destination.index);
+  };
+
   return (
-    <div className={styles.bl_task_board}>
-      {taskCards.map((taskCard) => (
-        <TaskCard key={taskCard.id} taskCard={taskCard} removeTaskCard={removeTaskCard} />
-      ))}
-      <AddTaskCardBtn addTaskCard={addTaskCard} />
-    </div>
+    <DragDropContext onDragEnd={onDragEndSortTasks}>
+      <Droppable droppableId='taskCardDroppable' direction='horizontal'>
+        {(provided) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <div className={styles.bl_task_board} {...provided.droppableProps} ref={provided.innerRef}>
+            {taskCards.map((taskCard, index) => (
+              <TaskCard key={taskCard.id} index={index} taskCard={taskCard} removeTaskCard={removeTaskCard} />
+            ))}
+            {provided.placeholder}
+            <AddTaskCardBtn addTaskCard={addTaskCard} />
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };

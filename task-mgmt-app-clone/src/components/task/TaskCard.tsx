382d2316bda5cfa14d5f4b4e5560f901fbc40c58
Symthy <sympathia3104@gmail.com>
2022-06-05 18:56:58 +0900

    add: display task empty

--- task-mgmt-app-clone/src/components/task/TaskCard.tsx
+++ task-mgmt-app-clone/src/components/task/TaskCard.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { v4 as uuid } from 'uuid';
+import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import { DeleteTaskCardBtn } from './button/DeleteTaskCardBtn';
-import { TaskAddInputForm } from './TaskAddInputForm';
 import { TaskCardTitle } from './TaskCardTitle';
 import styles from './TaskCard.module.scss';
-import { TaskCardData, TaskData } from './type';
+import { TaskCardData } from './type';
 import { Tasks } from './Tasks';
 
 type TaskCardProps = {
@@ -14,51 +12,26 @@ type TaskCardProps = {
   removeTaskCard: (id: string) => void;
 };
 
-export const TaskCard = ({ index, taskCard, removeTaskCard }: TaskCardProps) => {
-  const [tasks, setTasks] = useState<TaskData[]>([]);
-  const addTask = (newTaskName: string) => {
-    const taskId: string = uuid();
-    setTasks([
-      ...tasks,
-      {
-        id: taskId,
-        draggableId: `task-${taskId}`,
-        name: newTaskName,
-      },
-    ]);
-  };
-
-  const removeTask = (taskId: string) => {
-    setTasks(tasks.filter((t) => t.id !== taskId));
-  };
-
-  const reorderTasks = (startIndex: number, endIndex: number) => {
-    const remove = tasks.splice(startIndex, 1);
-    tasks.splice(endIndex, 0, remove[0]);
-    setTasks(tasks);
-  };
-
-  return (
-    <Draggable draggableId={taskCard.id} index={index}>
-      {(provided) => (
+export const TaskCard = ({ index, taskCard, removeTaskCard }: TaskCardProps) => (
+  <Draggable draggableId={taskCard.id} index={index}>
+    {(provided) => (
+      <div
+        className={styles.bl_task_card}
+        ref={provided.innerRef}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...provided.draggableProps}
+      >
         <div
-          className={styles.bl_task_card}
-          ref={provided.innerRef}
+          className={styles.bl_task_card_header}
           // eslint-disable-next-line react/jsx-props-no-spreading
-          {...provided.draggableProps}
+          {...provided.dragHandleProps}
         >
-          <div
-            className={styles.bl_task_card_header}
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            {...provided.dragHandleProps}
-          >
-            <TaskCardTitle />
-            <DeleteTaskCardBtn taskCard={taskCard} removeTaskCard={removeTaskCard} />
-          </div>
-          <TaskAddInputForm addTask={addTask} />
-          <Tasks tasks={tasks} removeTask={removeTask} reorderTasks={reorderTasks} />
+          <TaskCardTitle />
+          <DeleteTaskCardBtn taskCard={taskCard} removeTaskCard={removeTaskCard} />
         </div>
-      )}
-    </Draggable>
-  );
-};
+
+        <Tasks />
+      </div>
+    )}
+  </Draggable>
+);
--- task-mgmt-app-clone/src/components/task/Tasks.tsx
+++ task-mgmt-app-clone/src/components/task/Tasks.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
+import { v4 as uuid } from 'uuid';
 import { Task } from './Task';
 import { TaskData } from './type';
 import styles from './Tasks.module.scss';
+import { TaskAddInputForm } from './TaskAddInputForm';
 
-type TaskProps = {
-  tasks: TaskData[];
-  removeTask: (id: string) => void;
-  reorderTasks: (startIdx: number, endIdx: number) => void;
-};
+export const Tasks = () => {
+  const [tasks, setTasks] = useState<TaskData[]>([]);
+  const addTask = (newTaskName: string) => {
+    const taskId: string = uuid();
+    setTasks([
+      ...tasks,
+      {
+        id: taskId,
+        draggableId: `task-${taskId}`,
+        name: newTaskName,
+      },
+    ]);
+  };
 
-export const Tasks = ({ tasks, removeTask, reorderTasks }: TaskProps) => {
+  const removeTask = (taskId: string) => {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  };
+
+  const reorderTasks = (startIndex: number, endIndex: number) => {
+    const remove = tasks.splice(startIndex, 1);
+    tasks.splice(endIndex, 0, remove[0]);
+    setTasks(tasks);
+  };
+
   const onDragEndSortTasks = (result: DropResult) => {
     if (!result.destination) {
       return;
@@ -19,22 +38,29 @@ export const Tasks = ({ tasks, removeTask, reorderTasks }: TaskProps) => {
   };
 
   return (
-    <div className={styles.bl_tasks}>
-      <DragDropContext onDragEnd={onDragEndSortTasks}>
-        <Droppable key='taskDroppable' droppableId='taskDroppable'>
-          {(provided) => (
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {tasks.map((task, i) => (
-                <div key={task.id}>
-                  <Task index={i} task={task} removeTask={removeTask} />
+    <>
+      <TaskAddInputForm addTask={addTask} />
+      <div className={styles.bl_tasks}>
+        {tasks.length === 0 ? (
+          <p className={styles.el_empty_text}>empty</p>
+        ) : (
+          <DragDropContext onDragEnd={onDragEndSortTasks}>
+            <Droppable key='taskDroppable' droppableId='taskDroppable'>
+              {(provided) => (
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {tasks.map((task, i) => (
+                    <div key={task.id}>
+                      <Task index={i} task={task} removeTask={removeTask} />
+                    </div>
+                  ))}
+                  <div>{provided.placeholder}</div>
                 </div>
-              ))}
-              <div>{provided.placeholder}</div>
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        )}
+      </div>
+    </>
   );
 };
